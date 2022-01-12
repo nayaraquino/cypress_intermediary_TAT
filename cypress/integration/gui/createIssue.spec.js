9ef8eaa0974f677efbb8dfba76fdbfa1784937ae
Nayara Aquino <nayaraquino7@gmail.com>
2022-01-12 10:56:40 -0300

    lession 4 - intermediary tests

---
+++ cypress/integration/gui/createIssue.spec.js
@@ -0,0 +1,25 @@
+const faker = require('faker')
+
+describe('Create issue', () => {
+
+    const issue = {
+        title: `issue-${faker.datatype.uuid()}`,
+        description: faker.datatype.words(3),
+        project: {
+            name: `project-${faker.datatype.uuid()}`,
+            description: faker.datatype.words(5)
+        }
+    }
+
+    beforeEach(() => {
+        cy.login();
+        cy.gui_createProject(issue.project);
+    });
+
+    it('Successfully', () => {
+        cy.gui_createIssue(issue)
+        cy.get('.issue-details')
+        .should('contain', issue.title)
+        .and('contain', issue.description)
+    });
+});
